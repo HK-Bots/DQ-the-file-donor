bce0b7ef8f01ba6f5bf5947372bbac01c24798c9
Harmish77 <107766922+Harmish77@users.noreply.github.com>
2023-07-15 17:49:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'False')), False)
-HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', "https://t.me/c/1845700490/3")
+HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', "https://t.me/main_movie_hub/40")
 VERIFY2_URL = environ.get('VERIFY2_URL', "greymatterslinks.in")
 VERIFY2_API = environ.get('VERIFY2_API', "c762b2ab920b006a9bf7eed729589026d1da16b6")
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'greymatterslinks.in')
